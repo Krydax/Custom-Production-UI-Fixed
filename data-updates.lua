82a1710da2193fbb80938567b4609d5b6b17e7f1
Krydax <93162034+Krydax@users.noreply.github.com>
2023-01-14 20:37:29 -0500

    Initial commit

---
+++ data-updates.lua
@@ -0,0 +1,4 @@
+data.raw["utility-constants"]["default"].select_group_row_count = 10
+data.raw["utility-constants"]["default"].select_slot_row_count = 16
+data.raw["utility-constants"]["default"].inventory_width = 12
+data.raw["gui-style"]["default"].inventory_scroll_pane.minimal_width = data.raw["utility-constants"]["default"].inventory_width * 40 + 12
