516ea30e18366d1da8ee05baa5e72a0493f0430d
Krydax <93162034+Krydax@users.noreply.github.com>
2023-01-18 19:38:32 -0500

    options

--- data-updates.lua
+++ data-updates.lua
@@ -1,4 +1,10 @@
-data.raw["utility-constants"]["default"].select_group_row_count = 10
-data.raw["utility-constants"]["default"].select_slot_row_count = 16
-data.raw["utility-constants"]["default"].inventory_width = 12
-data.raw["gui-style"]["default"].inventory_scroll_pane.minimal_width = data.raw["utility-constants"]["default"].inventory_width * 40 + 12
+if settings.startup["inventory-width"].value then
+    data.raw["utility-constants"]["default"].inventory_width = 12
+    data.raw["gui-style"]["default"].inventory_scroll_pane.minimal_width = data.raw["utility-constants"]["default"].inventory_width * 40 + 12
+end
+if settings.startup["slots-width"].value then
+    data.raw["utility-constants"]["default"].select_slot_row_count = 17
+end
+if settings.startup["group-width"].value then
+    data.raw["utility-constants"]["default"].select_group_row_count = 10
+end
---
+++ settings.lua
@@ -0,0 +1,23 @@
+data:extend({
+    {
+        type = "bool-setting",
+        name = "inventory-width",
+        setting_type = "startup",
+        default_value = true,
+        order = "aa"
+    },
+    {
+        type = "bool-setting",
+        name = "slots-width",
+        setting_type = "startup",
+        default_value = true,
+        order = "ab"
+    },
+    {
+        type = "bool-setting",
+        name = "group-width",
+        setting_type = "startup",
+        default_value = true,
+        order = "ac"
+    }
+})
