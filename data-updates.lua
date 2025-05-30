70e9518bfd06232d35d8a869b15df281525124ed
setazer <mail@setazer.me>
2025-05-30 18:14:34 +0500

    Additional slot customizations with 2.0.53

--- data-updates.lua
+++ data-updates.lua
@@ -1,9 +1,12 @@
-if settings.startup["inventory-width"].value then
-    data.raw["utility-constants"]["default"].inventory_width = 12
-end
-if settings.startup["slots-width"].value then
-    data.raw["utility-constants"]["default"].select_slot_row_count = 17
-end
-if settings.startup["group-width"].value then
-    data.raw["utility-constants"]["default"].select_group_row_count = 10
-end
+data.raw["utility-constants"]["default"].inventory_width = settings.startup["inventory-width"].value
+data.raw["utility-constants"]["default"].select_slot_row_count = settings.startup["slots-width"].value
+data.raw["utility-constants"]["default"].select_group_row_count = settings.startup["group-width"].value
+
+-- new vars in 2.0.53, won't change anything in versions prior
+data.raw["utility-constants"]["default"].logistic_slots_per_row = settings.startup["logistic-width"].value
+data.raw["utility-constants"]["default"].crafting_queue_slots_per_row = settings.startup["crafting-width"].value
+data.raw["utility-constants"]["default"].trash_inventory_width = settings.startup["trash-width"].value
+data.raw["utility-constants"]["default"].blueprint_big_slots_per_row = settings.startup["blueprint-big-width"].value
+data.raw["utility-constants"]["default"].blueprint_small_slots_per_row = settings.startup["blueprint-small-width"].value
+
+
