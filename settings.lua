8bf789f5ddf7f06cc11be3a45f12a023499aa3fa
Eulenberg <letsplaymygames@gmail.com>
2025-07-07 18:19:35 +0200

    Changes new Settings to vanilla Default

--- settings.lua
+++ settings.lua
@@ -24,35 +24,35 @@ data:extend({
         type = "int-setting",
         name = "logistic-width",
         setting_type = "startup",
-        default_value = 12,
+        default_value = 10,
         order = "ac"
     },
     {
         type = "int-setting",
         name = "crafting-width",
         setting_type = "startup",
-        default_value = 12,
+        default_value = 10,
         order = "ac"
     },
     {
         type = "int-setting",
         name = "trash-width",
         setting_type = "startup",
-        default_value = 12,
+        default_value = 10,
         order = "ac"
     },
     {
         type = "int-setting",
         name = "blueprint-big-width",
         setting_type = "startup",
-        default_value = 10,
+        default_value = 6,
         order = "ac"
     },
     {
         type = "int-setting",
         name = "blueprint-small-width",
         setting_type = "startup",
-        default_value = 17,
+        default_value = 10,
         order = "ac"
     }
 })
